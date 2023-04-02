8e68e6489fdcc91d064cb78b1d36fd1455ffe455
vishal-dev4 <vc4772845@gmail.com>
2023-04-02 22:12:56 +0530

    everything is almost done

---
+++ scripts/address.js
@@ -0,0 +1,99 @@
+let cartArr = JSON.parse(localStorage.getItem("cartdata"))||[]
+let subtotal = document.getElementById("sub_total");
+let shippingfee = document.getElementById("shipping_fee");
+let carttotal = document.getElementById("cart_total");
+let continuebtn = document.getElementById("placeorder");
+let alladres = document.getElementById("all_address");
+
+let form = document.querySelector("form");
+let name = document.getElementById("name");
+let mobile = document.getElementById("mobile");
+let pincode = document.getElementById("pincode");
+let area = document.getElementById("area");
+let town = document.getElementById("town");
+let city = document.getElementById("city");
+let state = document.getElementById("state")
+
+let allAddress=JSON.parse(localStorage.getItem("address"))||[];
+form.addEventListener("submit",(e)=>{
+e.preventDefault();
+let obj={
+    name: name.value,
+    mobile: mobile.value,
+    pincode: pincode.value,
+    area: area.value,
+    town: town.value,
+    city: city.value,
+    state: state.value
+}
+  allAddress.push(obj);
+  localStorage.setItem("address",JSON.stringify(allAddress))
+  showaddress();
+  console.log(allAddress);
+})
+
+//showing address on address page
+showaddress();
+function showaddress(){
+    alladres.innerHTML="";
+   allAddress.forEach(element => {
+    let box = document.createElement("div");
+    let div1 = document.createElement("div");
+    let div2 = document.createElement("div");
+    let btn = document.createElement("input");
+    btn.setAttribute("type","radio");
+    let name = document.createElement("p");
+    name.innerText=element.name;
+    let area = document.createElement("p");
+    area.innerText=`${element.area}`;
+    let town = document.createElement("p");
+    town.innerText=`, ${element.town}`;
+    let city = document.createElement("p");
+    city.innerText=`, ${element.city}`;
+    let state = document.createElement("p");
+    state.innerText=element.state;
+    let pincode = document.createElement("p");
+    pincode.innerText = `-${element.pincode}`;
+    let mobile = document.createElement("p");
+    mobile.innerText=`Mobile: ${element.mobile}`;
+    div1.append(btn,name);
+    div2.append(area,town,city,state,pincode);
+    box.append(div1,div2,mobile);
+    alladres.append(box);
+   });
+}
+
+console.log(cartArr);
+//showing prise here
+showprice();
+ function showprice(){
+    let sum=0;
+    for(let i=0;i<cartArr.length;i++){
+     sum+=cartArr[i].price*cartArr[i].quantity;
+    }
+    subtotal.innerText =0;
+ 
+     let alltotal=0;
+    if(sum>=500){
+     shippingfee.innerText = 0;
+     alltotal=sum;
+     carttotal.innerText = sum;
+    }else if(sum<500){
+     carttotal.innerText = (sum+40);
+     alltotal=(sum+40);
+     shippingfee.innerText = 40;
+    }
+}
+
+function showcart(){
+    if(cartArr.length===0){
+    subtotal.innerText = 0;
+    shippingfee.innerText = 0;
+    carttotal.innerText = 0;
+    }
+  }
+showcart();
+
+continuebtn.addEventListener("click",()=>{
+    window.location.href = "../HTML/payment.html";
+  })
---
+++ scripts/cart.js
@@ -0,0 +1,150 @@
+let cartArr = JSON.parse(localStorage.getItem("cartdata"))||[]
+console.log(cartArr);
+
+let slide = document.getElementById("slide");
+let images=["../images/offer1.png","../images/offer2.png","../images/offer3.png"];
+let i=0;
+showoffer(i);
+
+//showing offers here
+function showoffer(){
+    slide.innerHTML="";
+    let button1 = document.createElement("button");
+    button1.innerText="<";
+    button1.addEventListener("click",()=>{
+        
+        if(i==0){
+            i=images.length-1;
+            showoffer(i);
+        }else{
+            i--;
+            showoffer(i);
+        }
+        
+    })
+    let button2 = document.createElement("button");
+    button2.innerText=">";
+  button2.addEventListener("click",()=>{
+    console.log(i)
+    if(i==images.length-1){
+        i=0;
+        showoffer(i);
+    }else{
+        i++;
+      showoffer(i);
+    }
+     
+  })
+
+   let image=document.createElement("img");
+    image.setAttribute("src",images[i]);
+
+
+    slide.append(button1,image,button2);
+}
+
+
+
+//showing all product in cart page
+
+let totalitems = document.createElement("totalitems");
+let allitems = document.getElementById("items");
+let subtotal = document.getElementById("sub_total");
+let discount = document.getElementById("discount");
+let promoButton = document.getElementById("promoButton");
+let placeorder = document.getElementById("placeorder");
+let carttotal = document.getElementById("cart_total");
+let shippingfee = document.getElementById("shipping_fee");
+let inbag = document.getElementById("inbag");
+let couponcode = document.getElementById("couponcode");
+
+showitem();
+ function showitem(){
+    allitems.innerHTML="";
+    cartArr.forEach(function(ele) {
+     let item = document.createElement("div");
+      let image = document.createElement("img");
+      image.setAttribute("src",ele.image);
+      let div = document.createElement("div");
+       let brand = document.createElement("h4");
+       brand.innerText=ele.brand;
+       let about = document.createElement("p");
+       about.innerText=ele.about;
+       let category = document.createElement("p");
+       category.innerText= ele.category;
+       let price = document.createElement("h4");
+       price.innerText = `₹${ele.price}`;
+       
+       let button = document.createElement("button");
+       button.innerText="X";
+        button.addEventListener("click",()=>{
+            Cart = cartArr.filter((element)=>{
+            return element.id!==ele.id;
+        })
+        localStorage.setItem("cartdata",JSON.stringify(Cart));
+        showcart();
+        showitem();
+        })
+
+       div.append(brand,about,category,price);
+      item.append(image,div,button);
+     allitems.append(item);
+    });
+
+    //calculating price here
+    //calculate total price here
+   let sum=0;
+   for(let i=0;i<cartArr.length;i++){
+    sum+=cartArr[i].price*cartArr[i].quantity;
+   }
+   subtotal.innerText = sum;
+
+    let alltotal=0;
+   if(sum>=500){
+    shippingfee.innerText = 0;
+    alltotal=sum;
+    carttotal.innerText = sum;
+   }else if(sum<500){
+    carttotal.innerText = (sum+40);
+    alltotal=(sum+40);
+    shippingfee.innerText = 40;
+   }
+
+    //applying promocode here
+    promoButton.addEventListener("click",()=>{
+        if(couponcode.value=="flat50"){
+            carttotal.innerText = (alltotal-50);
+            discount.innerText = 50;
+        }
+        else if(couponcode.value=="flat100"){
+            carttotal.innerText = (alltotal-100);
+            discount.innerText = 100;
+        }
+        else if(couponcode.value==""){
+            carttotal.innerText = alltotal;
+            discount.innerText=0;
+        }
+        else{
+            alert("Enter Valid Promo Code");
+        }
+       })
+    
+ }
+
+ function showcart(){
+    if(cartArr.length===0){
+     totalitems.innerText = "";
+    inbag.innerText = "Bag Is Empety! Add Some Items in the bag"
+    subtotal.innerText = 0;
+    discount.innerText = 0;
+    shippingfee.innerText = 0;
+    carttotal.innerText = 0;
+   }else if(cartArr.length>0){
+    totalitems.innerText = cartArr.length;
+   }
+  }
+  showcart();
+
+  placeorder.addEventListener("click",()=>{
+    window.location.href = "../HTML/address.html";
+  })
